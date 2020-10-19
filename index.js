c50b714f80f1169be87f89488f0383844f2e70ef
Manikant Singh <mani2474695@gmail.com>
2020-10-19 11:42:37 +0530

    Added nodemon express and mongoose

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const server = express();
+const port = 8000;
+
+server.get('/', (req, res) => {
+    res.status(200).json({
+        "message": "Serever started ",
+        "goal": 'start your work'
+    })
+})
+server.use('/', (req, res, next) => {
+    res.status(404).json({
+        'error': 'this is error 404'
+    })
+})
+
+
+server.listen(port, (error => {
+    if (error) { console.log(`Error occured in making the server`) }
+    else { console.log(`Serever is running at port${port}`) }
+}))
