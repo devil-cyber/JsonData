370ac9f4c926655982e6873298c98f42c681f42b
Manikant Singh <mani2474695@gmail.com>
2020-10-19 14:31:29 +0530

    Solved issue with .env file

--- config/mongo_client.js
+++ config/mongo_client.js
@@ -1,34 +1,27 @@
-const dotenv = require("dotenv");
-dotenv.config();
-console.log(process.env.PORT)
-const data = require('../resource');
-console.log(data);
-const { MongoClient } = require('mongodb');
+const dotenv = require("dotenv").config({ path: "../.env" });
+
+const { MongoClient } = require("mongodb");
 
 async function listDatabases(client) {
     databasesList = await client.db().admin().listDatabases();
 
     console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+    databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
 
 async function main() {
-    const uri = data;
-    console.log(uri);
+    const uri = process.env.URI;
     const client = new MongoClient(uri, { useUnifiedTopology: true });
     try {
         await client.connect();
         await listDatabases(client);
-    }
-    catch (error) {
+    } catch (error) {
         console.log(`error while connecting : ${error}`);
-    }
-    finally {
+    } finally {
         await client.close();
     }
-
 }
 
-main().catch(err => {
+main().catch((err) => {
     console.log(err);
-})
+});
