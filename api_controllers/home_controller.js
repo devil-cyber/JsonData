9804fcd16cbe0e6913b63acf298a457c0d1e9fc1
Manikant Singh <mani2474695@gmail.com>
2020-10-20 18:51:50 +0530

    added homepage

--- api_controllers/home_controller.js
+++ api_controllers/home_controller.js
@@ -1,6 +1,6 @@
 module.exports.home = function (req, res) {
     return res.status(200).json({
-        "message": 'This is our API',
-        "goal": 'This is our Goal'
+        message: 'This is a fake json data generator Api built by Manikant Kumar for Open Source | http://manikant.codes | Happy Coding',
+
     })
 }
